02c118e974a8bc957781ff84776224bd2c18ecb0
Khaled Adad <khaledadad@lewisu.edu>
2022-12-26 23:21:12 -0600

    refactor render and add sample items to db

--- food/views.py
+++ food/views.py
@@ -6,12 +6,13 @@ from django.template import loader
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template('food/index.html')
+    # template = loader.get_template('food/index.html')
     context = {
-
+        'item_list': item_list,
     }
     # return HttpResponse(item_list)
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'food/index.html', context)
 
 def item(request):
     return HttpResponse('This is an item view')
